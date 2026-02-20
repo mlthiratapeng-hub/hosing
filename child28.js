888e3dfb31b42e62d0701e940da2ee56ad706466
mlthiratapeng-hub <mlthiratapeng@gmail.com>
2026-02-20 21:08:47 +0700

    Create child28.js

---
+++ child28.js
@@ -0,0 +1,19 @@
+const { Client, GatewayIntentBits } = require("discord.js");
+const axios = require("axios");
+
+const client = new Client({
+    intents: [GatewayIntentBits.Guilds, GatewayIntentBits.DirectMessages],
+});
+
+client.once("ready", async () => {
+    console.log(`Child ready: ${client.user.tag}`);
+
+    if (!global.childBots) global.childBots = [];
+    global.childBots.push(client);
+
+    await axios.post("http://localhost:3000/register", {
+        name: client.user.tag
+    });
+});
+
+client.login(process.env.CHILD28_TOKEN);
