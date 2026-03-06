4de304dd7321cbbdb84adf82fa8a4f7ef9442292
หาเลขที่บ้าน <mlthiratapeng@gmail.com>
2026-03-06 21:18:58 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,30 @@
 import discord
 from discord.ext import commands
 import os
-import asyncio
 
 TOKEN = os.getenv("TOKEN")
 
-intents = discord.Intents.all()
+intents = discord.Intents.default()
+intents.message_content = True
 
 bot = commands.Bot(command_prefix="!", intents=intents)
+
+async def load_cogs():
+    await bot.load_extension("cogs.rep")
 
 @bot.event
 async def on_ready():
     print(f"Logged in as {bot.user}")
-    await bot.tree.sync()
-
-async def load_cogs():
-    for file in os.listdir("./cogs"):
-        if file.endswith(".py"):
-            await bot.load_extension(f"cogs.{file[:-3]}")
+    try:
+        synced = await bot.tree.sync()
+        print(f"Synced {len(synced)} commands")
+    except Exception as e:
+        print(e)
 
 async def main():
     async with bot:
         await load_cogs()
         await bot.start(TOKEN)
 
+import asyncio
 asyncio.run(main())
