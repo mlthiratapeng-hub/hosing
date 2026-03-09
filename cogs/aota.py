b0f0446d76f29a7983a6a720e492e4f627bcffa9
หาเลขที่บ้าน <mlthiratapeng@gmail.com>
2026-03-09 12:21:47 +0700

    Update aota.py

--- cogs/aota.py
+++ cogs/aota.py
@@ -1,14 +1,15 @@
 import discord
 from discord.ext import commands
 from discord import app_commands
+import asyncio
 
 class Aota(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @app_commands.command(name="fack", description="ส่งข้อความ")
+    @app_commands.command(name="aota", description="Trigger message")
     @app_commands.describe(
-        message="ข้อความที่ต้องการทริกเกอร์",
+        message="ข้อความที่จะทริกเกอร์",
         amount="จำนวน (สูงสุด 3)"
     )
     async def aota(self, interaction: discord.Interaction, message: str, amount: int):
@@ -20,11 +21,13 @@ class Aota(commands.Cog):
 
         # ตอบเฉพาะคนสั่ง
         await interaction.response.send_message(
-            f"🚀 Triggering `{message}` x{amount}",
+            f"🚀 กำลัง Trigger `{message}` x{amount}",
             ephemeral=True
         )
 
         channel = interaction.channel
+
+        await asyncio.sleep(1)
 
         # ส่งข้อความจริงให้ทุกคนเห็น
         for i in range(amount):
