1fe391b1c0d812693e776b140fd28ffda6942898
หาเลขที่บ้าน <mlthiratapeng@gmail.com>
2026-08-05 21:47:43 +0700

    Create BootReceiver.kt

---
+++ BootReceiver.kt
@@ -0,0 +1,13 @@
+package com.yourstealth.app
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class BootReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context?, intent: Intent?) {
+        if (Intent.ACTION_BOOT_COMPLETED == intent?.action) {
+            context?.startForegroundService(Intent(context, BackgroundService::class.java))
+        }
+    }
+}
